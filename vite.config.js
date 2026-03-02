07bbf877c9e373c35dc24f99c920dada5242bb94
dmorton714 <danny.morton714@gmail.com>
2026-03-02 15:40:25 -0500

    built

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "https://github.com/dmorton714/personalSite/",
   plugins: [
     vue(),
     vueDevTools(),
