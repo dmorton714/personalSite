1331982a5f065a6db44571398b5b05e5830125d1
dmorton714 <danny.morton714@gmail.com>
2026-03-02 15:45:42 -0500

    update files

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://github.com/dmorton714/personalSite/",
+  base: "/personalSite/",
   plugins: [
     vue(),
     vueDevTools(),
